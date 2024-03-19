d7d49fa7826b08f64318806ea8aea4c992204aa2
labrot <mister.labrot@gmail.com>
2024-03-19 11:47:13 +0100

    Expé OK

--- Views/view_tickets.php
+++ Views/view_tickets.php
@@ -1,7 +1,14 @@
 <?php 
 if(isset($tickets_spec)){
+    if($tickets_spec == false){
 ?>
-    <h2 class="titre_expe">Tickets commande n° <?php echo $tickets_spec["ID_COMMANDE"]; ?> :</h2>
+        <h3 id="titre_centre">Il n'y a pas de tickets pour la commande n°<?php echo $id; ?></h3>
+        <button type="reset" id="btn_retour_centre" class="btn btn-danger"><a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" style="text-decoration:none;color: #FFFFFF">Retour</a></button>
+<?php
+    }
+    else{
+?>
+    <h2 class="titre_expe">Tickets commande n° <?php echo $id; ?> :</h2>
     <table id="detail_commande" class="table table-dark table-striped table-bordered">
         <thead>
             <tr>
@@ -12,13 +19,22 @@ if(isset($tickets_spec)){
             </tr>
         </thead>
         <tbody>
+<?php
+        foreach($tickets_spec as $ticket){
+            $employe = getNameEmploye($ticket["ID_EMPLOYE"]);
+?>
             <tr>
-                <td><?php echo date('d/m/Y', strtotime($tickets_spec["DATE_TICKET"])); ?></td>
-                <td><?php echo $tickets_spec["MOTIF_TICKET"]; ?></td>
-                <td><?php echo $tickets_spec["ID_EMPLOYE"]; ?></td>
-                <td><?php echo $tickets_spec["ID_COMMANDE"]; ?></td>
+                <td><?php echo date('d/m/Y', strtotime($ticket["DATE_TICKET"])); ?></td>
+                <td><?php echo $ticket["MOTIF_TICKET"]; ?></td>
+                <td><?php echo $employe["NOM_UTILISATEUR"] . " " . $employe["PRENOM_UTILISATEUR"]; ?></td>
+                <td><?php echo $ticket["ID_COMMANDE"]; ?></td>
             </tr>
+<?php
+        }
+?>
         </tbody>
     </table>
+    <button type="reset" id="btn_retour" class="btn btn-danger"><a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" style="text-decoration:none;color: #FFFFFF">Retour</a></button>
 <?php
+    }
 }
